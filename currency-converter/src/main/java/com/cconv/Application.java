6c4b62f392f6d869d97ce127c1c89e10c940a689
Alessia <azzolina.alessia@gmail.com>
2025-02-19 15:57:15 +0100

    initial commit

---
+++ currency-converter/src/main/java/com/cconv/Application.java
@@ -0,0 +1,19 @@
+package com.cconv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot application start point
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * start point
+	 *
+	 * @param args system arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
